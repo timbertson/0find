c08fa36da2b8921b0f0a886f16abfc6e5c9d3a12
gfxmonk <tim@gfxmonk.net>
2011-01-21 22:58:31 +1100

    added special-case for package and local implementations

--- zerofind.py
+++ zerofind.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 
 import sys
-import zerofind
+import os
 
 from zeroinstall import helpers
 from zeroinstall import zerostore
 
 def find(url):
-	selections = helpers.ensure_cached(url)
+	selections = helpers.ensure_cached(url, command='')
 	if not selections:
 		return None
 	selection = selections.selections[url]
 
+	if selection.id.startswith("package:"):
+		print >> sys.stderr, "Package implementation: %s" % (selection.id,)
+		return None
+	if os.path.exists(selection.id):
+		return selection.id
 	return zerostore.Stores().lookup(selection.id)
 
 if __name__ == '__main__':
 	url = sys.argv[1]
 	cache_dir = find(url)
 	if not cache_dir:
-		print >> sys.stderr, "Cancelled."
+		print >> sys.stderr, "Failed."
 		sys.exit(1)
 	print cache_dir
 
